48f55317bd706ed32ae5f0057ba70f479392f763
Wilco Greven <greven@kde.org>
2002-01-25 18:56:03 +0000

    It was written in 2002.
    svn path=/trunk/kdegraphics/kfile-plugins/; revision=133845

--- ps/kfile_ps.cpp
+++ ps/kfile_ps.cpp
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
- * Copyright (C) 2001, 2002 Rolf Magnus <ramagnus@kde.org>
+ * Copyright (C) 2002 Wilco Greven <greven@kde.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
