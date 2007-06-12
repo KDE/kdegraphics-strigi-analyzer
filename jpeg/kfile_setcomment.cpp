298411ac153fa6094b5d13a9cc91ab44532ed801
Pino Toscano <pino@kde.org>
2007-06-12 17:03:50 +0000

    a global config.h is not needed here
    svn path=/trunk/KDE/kdegraphics/kfile-plugins/; revision=674600

--- jpeg/kfile_setcomment.cpp
+++ jpeg/kfile_setcomment.cpp
@@ -41,7 +41,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
-#include "config.h"
 
 extern int safe_copy_and_modify( const char * original_filename, const char * comment );
     
