d7bb4dd0947f07b65192b66a594e28f0866b0595
Volker Krause <vkrause@kde.org>
2009-06-20 17:42:00 +0000

    SVN_SILENT replace no longer valid email address
    svn path=/trunk/KDE/kdegraphics/strigi-analyzer/; revision=984410

--- pnm/kfile_pnm.h
+++ pnm/kfile_pnm.h
@@ -1,5 +1,5 @@
 /* This file is part of the KDE project
- * Copyright (C) 2003 Volker Krause <volker.krause@rwth-aachen.de>
+ * Copyright (C) 2003 Volker Krause <vkrause@kde.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
